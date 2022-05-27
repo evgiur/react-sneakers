c2d925c59466bdc56587ebddba9125b9eee3029d
evgiur <evgiur@gmail.com>
2022-05-27 17:56:21 +0300

    html-css

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           
           <div>
             <h3>React Sneakers</h3>
-            <p>Best sneakers shop</p>
+            <p className="description">Best sneakers shop</p>
           </div>
 
         </div>
@@ -29,62 +29,71 @@ function App() {
       
       {/*CONTENT*/}
       <div className="content">
-        <h1 className="h1"> All sneakers</h1>
+        <div className="search-box">
+          <h1 className="h1">All sneakers</h1>
+          <div className="search-line">
+            <img src="img/search.svg" alt="Search?"></img>
+            <input placeholder="Search"></input>
+          </div>
+        </div>
         
-        <div className="itemCard">
-          <img width={133} height={112} src="img/sneakers/1.jpg" alt="Sneakers"/>
-          <h5>Male sneakers Nike Blazer Mid Suede</h5>
-          <div className="cardTop">
-            <div className="cardPrice">
-              <span>Price: </span>
-              <b>200$</b>              
+        <div className="sneakers">
+          <div className="itemCard">
+            <img width={133} height={112} src="img/sneakers/1.jpg" alt="Sneakers"/>
+            <h5>Male sneakers Nike Blazer Mid Suede</h5>
+            <div className="cardTop">
+              <div className="cardPrice">
+                <span>Price: </span>
+                <b>200 $</b>              
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+              </button>
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-            </button>
           </div>
-        </div>
 
-        <div className="itemCard">
-          <img width={133} height={112} src="img/sneakers/2.jpg" alt="Sneakers"/>
-          <h5>Male sneakers Nike Blazer Mid Suede</h5>
-          <div className="cardTop">
-            <div className="cardPrice">
-              <span>Price: </span>
-              <b>200$</b>              
+          <div className="itemCard">
+            <img width={133} height={112} src="img/sneakers/2.jpg" alt="Sneakers"/>
+            <h5>Male sneakers Nike Blazer Mid Suede</h5>
+            <div className="cardTop">
+              <div className="cardPrice">
+                <span>Price: </span>
+                <b>200 $</b>              
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+              </button>
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-            </button>
           </div>
-        </div>
 
-        <div className="itemCard">
-          <img width={133} height={112} src="img/sneakers/3.jpg" alt="Sneakers"/>
-          <h5>Male sneakers Nike Blazer Mid Suede</h5>
-          <div className="cardTop">
-            <div className="cardPrice">
-              <span>Price: </span>
-              <b>200$</b>              
+          <div className="itemCard">
+            <img width={133} height={112} src="img/sneakers/3.jpg" alt="Sneakers"/>
+            <h5>Male sneakers Nike Blazer Mid Suede</h5>
+            <div className="cardTop">
+              <div className="cardPrice">
+                <span>Price: </span>
+                <b>200 $</b>              
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+              </button>
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-            </button>
           </div>
-        </div>
 
-        <div className="itemCard">
-          <img width={133} height={112} src="img/sneakers/4.jpg" alt="Sneakers"/>
-          <h5>Male sneakers Nike Blazer Mid Suede</h5>
-          <div className="cardTop">
-            <div className="cardPrice">
-              <span>Price: </span>
-              <b>200$</b>              
+          <div className="itemCard">
+            <img width={133} height={112} src="img/sneakers/4.jpg" alt="Sneakers"/>
+            <h5>Male sneakers Nike Blazer Mid Suede</h5>
+            <div className="cardTop">
+              <div className="cardPrice">
+                <span>Price: </span>
+                <b>200 $</b>              
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+              </button>
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-            </button>
           </div>
+
         </div>
 
 
