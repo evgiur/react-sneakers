e3454f74d994cf7231af2ae3d4a695749cca5545
evgiur <evgiur@gmail.com>
2022-09-29 11:51:06 +0300

    card-component-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="wrapper clear">
-      <div className="overlay">
+      <div style = {{display: 'none'}} className="overlay">
         <div className="drawer">
           <h2>Cart 
             <img
@@ -101,29 +101,7 @@ function App() {
         </div>
 
         <div className="sneakers">
-          <div className="itemCard">
-            <div className="favorite">
-              <img src="img/unliked.svg" alt="Unliked" />
-            </div>
-
-            <img
-              width={133}
-              height={112}
-              src="img/sneakers/1.jpg"
-              alt="Sneakers"
-            />
-            <h5>Male sneakers Nike Blazer Mid Suede</h5>
-            <div className="cardTop">
-              <div className="cardPrice">
-                <span>Price: </span>
-                <b>200 $</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
-              </button>
-            </div>
-          </div>
-
+          
           <div className="itemCard">
             <img
               width={133}
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+<div className="itemCard">
+    
+    <div className="favorite">
+        <img src="img/unliked.svg" alt="Unliked" />
+    </div>
+
+    <img
+        width={133}
+        height={112}
+        src="img/sneakers/1.jpg"
+        alt="Sneakers"
+    />
+    
+    <h5>Male sneakers Nike Blazer Mid Suede</h5>
+
+    <div className="cardTop">
+        <div className="cardPrice">
+            <span>Price: </span>
+            <b>200 $</b>
+        </div>
+    
+        <button className="button">
+            <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+        </button>
+    </div>
+</div>
