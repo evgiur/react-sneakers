3e72ec8c6c55596564611a3caca02f8a1a6ac803
evgiur <evgiur@gmail.com>
2022-10-02 16:27:25 +0300

    props-done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import Card from './components/Card.js';
 import Header from './components/Header.js';
 import CartDrawer from './components/CartDrawer.js';
 
-const arr = []
+const arr = [
+              {
+                title: 'Mens skeakers Nike Blazer Mid Suede', 
+                price: 199, 
+                imageUrl: './img/sneakers/1.jpg'
+              }, 
+              {
+                title: 'Mens skeakers Nike Air Max 270', 
+                price: 199, 
+                imageUrl: './img/sneakers/2.jpg'
+              }, 
+              {
+                title: 'Mens skeakers Nike Blazer Mid Suede', 
+                price: 199, 
+                imageUrl: './img/sneakers/3.jpg'
+              }, 
+              {
+                title: 'Mens skeakers Puma X Aka Boku Future Rider', 
+                price: 199, 
+                imageUrl: './img/sneakers/4.jpg'
+              }
+            ]
 
 function App() {
   return (
@@ -21,7 +42,11 @@ function App() {
         </div>
 
         <div className="sneakers">
-          <Card />
+          {
+            arr.map((obj) => (
+              <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl}/>
+            ))
+          }
         </div>
       </div>
     </div>
